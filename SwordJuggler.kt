226ed414e7c4363c3be4417bdb8eee1f45af6810
rain <chenu9999@gmail.com>
2021-06-22 15:04:13 +0800

    先決條件函數(require)

--- SwordJuggler.kt
+++ SwordJuggler.kt
@@ -1,5 +1,3 @@
-//import java.lang.IllegalStateException
-
 fun main() {
     var swordsJuggling: Int? = null
     val isJugglingProficient = (1..3).shuffled().last() == 3
@@ -12,7 +10,13 @@ fun main() {
         println(e)
     }
 
-    println("你耍了$swordsJuggling" + "把劍!")
+    if (swordsJuggling == null) {
+        println("玩家不會耍劍")  //多表達式寫法 此行刪除輸出結果較正確
+        swordsJuggling = 0
+    }
+    else {println("你耍了$swordsJuggling" + "把劍!")
+    }
+    juggleSwords(swordsJuggling)
 }
 
 fun proficiencyCheck(swordsJuggling: Int?) {
@@ -20,5 +24,6 @@ fun proficiencyCheck(swordsJuggling: Int?) {
     //checkNotNull:如果參數值為null 則拋出IllegalStateException異常 不為null則返回值
 }
 
-/*class UnskilledSwordJugglerException() :
-    IllegalStateException("玩家不會耍劍")*/
+fun juggleSwords(swordsJuggling: Int) {
+    require(swordsJuggling >= 3, {"至少要耍三把才厲害"})
+}
