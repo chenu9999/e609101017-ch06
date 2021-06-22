37e4b650e8f48024e3030407db5ab48b72f4f396
rain <chenu9999@gmail.com>
2021-06-22 14:16:42 +0800

    6-11增加耍劍邏輯

---
+++ SwordJuggler.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
+
+    println("你耍了$swordsJuggling" + "把劍!")
+}
