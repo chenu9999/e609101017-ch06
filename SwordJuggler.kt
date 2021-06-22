e101ecd20a4d4a14d864b31d432256c996d64988
rain <chenu9999@gmail.com>
2021-06-22 14:31:10 +0800

    c6-16處理異常,增加try/catch語句

--- SwordJuggler.kt
+++ SwordJuggler.kt
@@ -5,8 +5,12 @@ fun main() {
     val isJugglingProficient = (1..3).shuffled().last() == 3
     if (isJugglingProficient) swordsJuggling = 2
 
-    proficiencyCheck(swordsJuggling)
-    swordsJuggling = swordsJuggling!!.plus(1)
+    try {
+        proficiencyCheck(swordsJuggling)
+        swordsJuggling = swordsJuggling!!.plus(1)
+    } catch (e: Exception) {
+        println(e)
+    }
 
     println("你耍了$swordsJuggling" + "把劍!")
 }
