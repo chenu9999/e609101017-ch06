3beda332ad92e545ef416c1fadcff91d33902360
rain <chenu9999@gmail.com>
2021-06-22 14:11:35 +0800

    c6-10使用空合併運算子

--- Tavern.kt
+++ Tavern.kt
@@ -8,5 +8,7 @@ fun main() {
         println("I can't do that without crashing - beverage was null!")
     }
 
-    println(beverage)
+    val beverageServed: String = beverage ?: "Buttered Ale"
+    println(beverageServed)
+    //  空合併運算子(?:) 如果運算子左邊的求值結果為null 則採用右邊的結果值
 }
