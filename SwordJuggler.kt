0555fe949ee4837b3f8c43a5f130f9235f436d5f
rain <chenu9999@gmail.com>
2021-06-22 14:23:55 +0800

    c6-13拋出一個IllegalStateException

--- SwordJuggler.kt
+++ SwordJuggler.kt
@@ -1,11 +1,16 @@
+import java.lang.IllegalStateException
+
 fun main() {
     var swordsJuggling: Int? = null
     val isJugglingProficient = (1..3).shuffled().last() == 3
-    if (isJugglingProficient) {
-        swordsJuggling = 2
-    }
+    if (isJugglingProficient) swordsJuggling = 2  //if else句型省略
 
+    proficiencyCheck(swordsJuggling)
     swordsJuggling = swordsJuggling!!.plus(1)
 
     println("你耍了$swordsJuggling" + "把劍!")
+}
+
+fun proficiencyCheck(swordsJuggling: Int?) {
+    swordsJuggling ?: throw IllegalStateException("玩家不會耍劍")
 }
